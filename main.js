4e011bcba297d19304077f3bae2ed54cb47784bb
Blumecia <melissa.leroy.m@gmail.com>
2020-11-08 17:15:36 +0100

    Add function to generate mixed messages

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ let subject = ['Scorpio', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo',
 let verb = ['should fear', 'should take more time for', 'will miss', 'will hate', 'should listen more to', 'will spend a great time with'];
 let COD = ['their loved ones', 'their ennemies', 'their friends', 'their colleagues', 'their parents', 'their siblings', 'their partner', 'their teammates'];
 
-// Define randomly a subject for the sentence
-
-
-// Define randomly a verb for the sentence
-
-
-//Define randomly a COD for the sentence
-
-
 //Join the randomly generated pieces of sentence
+const generateSentence = () => {
 
-// Output a console.log with the mixed version
-console.log(mixedMessage);
+    // Define randomly a subject for the sentence
+    let randomSubject = subject[Math.floor(Math.random() * subject.length)]
+
+    // Define randomly a verb for the sentence
+    let randomVerb = verb[Math.floor(Math.random() * verb.length)]
+
+    //Define randomly a COD for the sentence
+    let randomCOD = COD[Math.floor(Math.random() * COD.length)]
+
+    let mixedMessage = randomSubject + ' ' + randomVerb + ' ' + randomCOD + ' today.';
+    console.log(mixedMessage);
+}
+
+generateSentence();
