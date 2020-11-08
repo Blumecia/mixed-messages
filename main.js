13a99d44a6b6f1a7c39d2bc9fd46bf2ca86384ec
Blumecia <melissa.leroy.m@gmail.com>
2020-11-08 14:20:45 +0100

    Add three possible pieces of sentence

---
+++ main.js
@@ -0,0 +1,18 @@
+//Create the different arrays for subject, verb and COD
+let subject = ['Scorpio', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+let verb = ['should fear', 'should take more time for', 'will miss', 'will hate', 'should listen more to', 'will spend a great time with'];
+let COD = ['their loved ones', 'their ennemies', 'their friends', 'their colleagues', 'their parents', 'their siblings', 'their partner', 'their teammates'];
+
+// Define randomly a subject for the sentence
+
+
+// Define randomly a verb for the sentence
+
+
+//Define randomly a COD for the sentence
+
+
+//Join the randomly generated pieces of sentence
+
+// Output a console.log with the mixed version
+console.log(mixedMessage);
